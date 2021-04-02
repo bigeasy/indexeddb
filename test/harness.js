292a850d0f892a0b60d141f3c2fb32ab409d3ff3
Alan Gutierrez <alan@prettyrobots.com>
2021-04-01 22:33:34 -0500

    Export to globals from test harness.
    Really, why fight it?

--- test/harness.js
+++ test/harness.js
@@ -1,18 +1,146 @@
 module.exports = async function (okay, name) {
     const fs = require('fs').promises
+    const assert = require('assert')
     const path = require('path')
+    const rmrf = require('../rmrf')
     const directory = path.join(__dirname, 'tmp', name)
-    if (fs.rm != null) {
-        await fs.rm(directory, { recursive: true, force: true })
-    } else {
-        await fs.rmdir(directory, { recursive: true })
-    }
+    await rmrf(process.version, fs, directory)
     await fs.mkdir(directory, { recursive: true })
     class Test {
     }
+    globalize(Test)
     const indexedDB = require('..').create({ directory })
-    global.indexedDB = indexedDB
-    okay.leak('indexedDB')
-    global.Test = Test
-    okay.leak('Test')
+    globalize(indexedDB, 'indexedDB')
+    globalize({ indexedDB }, 'window')
+    const scope = {}
+    function globalize (value, name = null) {
+        if (name == null) {
+            switch (typeof value) {
+            case 'function':
+                global[value.name] = value
+                okay.leak(value.name)
+            }
+        } else {
+            global[name] = value
+            okay.leak(name)
+        }
+    }
+    function test (f, name) {
+        scope.name = name
+        scope.count = 0
+        f()
+    }
+    globalize(test)
+    function assert_equals (actual, expected, message) {
+        okay.inc(1)
+        okay(actual, expected, `${scope.name} - assertion ${scope.count++}`)
+    }
+    globalize(assert_equals)
+    function assert_throws_js(constructor, func, description) {
+        try {
+            func.call(null)
+            assert(false, 'did not throw')
+        } catch (error) {
+            if (error instanceof assert.AssertionError) {
+                throw error
+            }
+            if (error.constructor !== constructor) {
+                console.log(error.stack)
+            }
+            okay(error.constructor === constructor, `${scope.name} - assertion ${scope.count++}`)
+            okay.inc(1)
+        }
+    }
+    globalize(assert_throws_js)
+    function assert_throws_dom(type, func, description) {
+        try {
+            func.call(null)
+        } catch (error) {
+            if (error instanceof assert.AssertionError) {
+                throw error
+            }
+            const names = {
+                INDEX_SIZE_ERR: 'IndexSizeError',
+                HIERARCHY_REQUEST_ERR: 'HierarchyRequestError',
+                WRONG_DOCUMENT_ERR: 'WrongDocumentError',
+                INVALID_CHARACTER_ERR: 'InvalidCharacterError',
+                NO_MODIFICATION_ALLOWED_ERR: 'NoModificationAllowedError',
+                NOT_FOUND_ERR: 'NotFoundError',
+                NOT_SUPPORTED_ERR: 'NotSupportedError',
+                INUSE_ATTRIBUTE_ERR: 'InUseAttributeError',
+                INVALID_STATE_ERR: 'InvalidStateError',
+                SYNTAX_ERR: 'SyntaxError',
+                INVALID_MODIFICATION_ERR: 'InvalidModificationError',
+                NAMESPACE_ERR: 'NamespaceError',
+                INVALID_ACCESS_ERR: 'InvalidAccessError',
+                TYPE_MISMATCH_ERR: 'TypeMismatchError',
+                SECURITY_ERR: 'SecurityError',
+                NETWORK_ERR: 'NetworkError',
+                ABORT_ERR: 'AbortError',
+                URL_MISMATCH_ERR: 'URLMismatchError',
+                QUOTA_EXCEEDED_ERR: 'QuotaExceededError',
+                TIMEOUT_ERR: 'TimeoutError',
+                INVALID_NODE_TYPE_ERR: 'InvalidNodeTypeError',
+                DATA_CLONE_ERR: 'DataCloneError'
+            }
+
+            const codes = {
+                IndexSizeError: 1,
+                HierarchyRequestError: 3,
+                WrongDocumentError: 4,
+                InvalidCharacterError: 5,
+                NoModificationAllowedError: 7,
+                NotFoundError: 8,
+                NotSupportedError: 9,
+                InUseAttributeError: 10,
+                InvalidStateError: 11,
+                SyntaxError: 12,
+                InvalidModificationError: 13,
+                NamespaceError: 14,
+                InvalidAccessError: 15,
+                TypeMismatchError: 17,
+                SecurityError: 18,
+                NetworkError: 19,
+                AbortError: 20,
+                URLMismatchError: 21,
+                QuotaExceededError: 22,
+                TimeoutError: 23,
+                InvalidNodeTypeError: 24,
+                DataCloneError: 25,
+
+                EncodingError: 0,
+                NotReadableError: 0,
+                UnknownError: 0,
+                ConstraintError: 0,
+                DataError: 0,
+                TransactionInactiveError: 0,
+                ReadOnlyError: 0,
+                VersionError: 0,
+                OperationError: 0,
+                NotAllowedError: 0
+            }
+
+            const codeNames = {}
+
+            for (const key in codes) {
+                codeNames[codes[key]] = key
+            }
+
+            okay({
+                name: error.name,
+                code: error.code
+            }, {
+                name: type,
+                code: codes[type],
+            }, `${scope.name} - assertion ${scope.count++}`)
+
+            if (typeof type == 'number') {
+                throw new Error
+            } else {
+                const name = type in codes ? codes[type] : type
+            }
+            okay.inc(1)
+        }
+    }
+    globalize(assert_throws_dom)
 }
--- test/idbfactory_cmp.t.js
+++ test/idbfactory_cmp.t.js
@@ -1,52 +1,6 @@
 require('proof')(0, async okay => {
-    const assert = require('assert')
-    const path = require('path')
-    const fs = require('fs').promises
-    const directory = path.join(__dirname, 'tmp', 'idbfactory_cmp')
-    if (fs.rm != null) {
-        await fs.rm(directory, { recursive: true, force: true })
-    } else {
-        await fs.rmdir(directory, { recursive: true })
-    }
-    await fs.mkdir(directory, { recursive: true })
+    await require('./harness')(okay, 'idbfactory_cmp')
 
-    const indexedDB = require('..').create({ directory })
-    const window = { indexedDB }
-    function assert_equals (actual, expected, message) {
-        okay.inc(1)
-        okay(actual, expected, message)
-    }
-    const scope = { name: null, count: 0 }
-    function assert_throws_dom(constructor, func, description) {
-        try {
-        } catch (error) {
-            if (error instanceof assert.AssertionError) {
-                throw error
-            }
-        }
-        okay.inc(1)
-    }
-    function assert_throws_js(constructor, func, description) {
-        try {
-            func.call(null)
-            assert(false, 'did not throw')
-        } catch (error) {
-            if (error instanceof assert.AssertionError) {
-                throw error
-            }
-            if (error.constructor !== constructor) {
-                console.log(error.stack)
-            }
-            okay(error.constructor === constructor, `${scope.name} - assertion ${scope.count++}`)
-            okay.inc(1)
-        }
-    }
-    function test (f, name) {
-        scope.name = name
-        scope.count = 0
-        f()
-    }
-    const self = {};
     test(function() {
         var greater = window.indexedDB.cmp(2, 1);
         var equal = window.indexedDB.cmp(2, 2);
