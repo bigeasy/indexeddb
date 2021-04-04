891c0c4c218a3432d27437331451b8565816eb96
Alan Gutierrez <alan@prettyrobots.com>
2021-04-04 12:44:08 -0500

    Use new test harness in generated tests.

--- test/idbfactory_cmp.t.js
+++ test/idbfactory_cmp.t.js
@@ -1,13 +1,14 @@
 require('proof')(0, async okay => {
     await require('./harness')(okay, 'idbfactory_cmp')
+    await harness(async function () {
+        test(function() {
+            var greater = window.indexedDB.cmp(2, 1);
+            var equal = window.indexedDB.cmp(2, 2);
+            var less = window.indexedDB.cmp(1, 2);
 
-    test(function() {
-        var greater = window.indexedDB.cmp(2, 1);
-        var equal = window.indexedDB.cmp(2, 2);
-        var less = window.indexedDB.cmp(1, 2);
-
-        assert_equals(greater, 1, "greater");
-        assert_equals(equal, 0, "equal");
-        assert_equals(less, -1, "less");
-    }, "IDBFactory.cmp()");
+            assert_equals(greater, 1, "greater");
+            assert_equals(equal, 0, "equal");
+            assert_equals(less, -1, "less");
+        }, "IDBFactory.cmp()");
+    })
 })
--- test/idbfactory_cmp2.t.js
+++ test/idbfactory_cmp2.t.js
@@ -1,33 +1,34 @@
 require('proof')(0, async okay => {
     await require('./harness')(okay, 'idbfactory_cmp2')
+    await harness(async function () {
+        test( function() {
+            assert_throws_js(TypeError, function() {
+                indexedDB.cmp();
+            });
+        }, "IDBFactory.cmp() - no argument");
 
-    test( function() {
-        assert_throws_js(TypeError, function() {
-            indexedDB.cmp();
-        });
-    }, "IDBFactory.cmp() - no argument");
+        test( function() {
+            assert_throws_dom("DataError", function() {
+                indexedDB.cmp(null, null);
+            });
+            assert_throws_dom("DataError", function() {
+                indexedDB.cmp(1, null);
+            });
+            assert_throws_dom("DataError", function() {
+                indexedDB.cmp(null, 1);
+            });
+        }, "IDBFactory.cmp() - null");
 
-    test( function() {
-        assert_throws_dom("DataError", function() {
-            indexedDB.cmp(null, null);
-        });
-        assert_throws_dom("DataError", function() {
-            indexedDB.cmp(1, null);
-        });
-        assert_throws_dom("DataError", function() {
-            indexedDB.cmp(null, 1);
-        });
-    }, "IDBFactory.cmp() - null");
-
-    test( function() {
-        assert_throws_dom("DataError", function() {
-            indexedDB.cmp(NaN, NaN);
-        });
-        assert_throws_dom("DataError", function() {
-            indexedDB.cmp(1, NaN);
-        });
-        assert_throws_dom("DataError", function() {
-            indexedDB.cmp(NaN, 1);
-        });
-    }, "IDBFactory.cmp() - NaN");
+        test( function() {
+            assert_throws_dom("DataError", function() {
+                indexedDB.cmp(NaN, NaN);
+            });
+            assert_throws_dom("DataError", function() {
+                indexedDB.cmp(1, NaN);
+            });
+            assert_throws_dom("DataError", function() {
+                indexedDB.cmp(NaN, 1);
+            });
+        }, "IDBFactory.cmp() - NaN");
+    })
 })
