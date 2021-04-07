026ba3ea1ff112c416c0a7e3136b01461c5feee5
Alan Gutierrez <alan@prettyrobots.com>
2021-04-07 02:48:08 -0500

    Put with explicit key.
    See #18.

---
+++ test/idbobjectstore_put2.wpt.t.js
@@ -0,0 +1,29 @@
+require('proof')(0, async okay => {
+    await require('./harness')(okay, 'idbobjectstore_put2')
+    await harness(async function () {
+        var db,
+          t = async_test(),
+          key = 1,
+          record = { property: "data" };
+
+        var open_rq = createdb(t);
+        open_rq.onupgradeneeded = function(e) {
+            db = e.target.result;
+            var objStore = db.createObjectStore("store");
+
+            objStore.put(record, key);
+        };
+
+        open_rq.onsuccess = function(e) {
+            var rq = db.transaction("store")
+                       .objectStore("store")
+                       .get(key);
+
+            rq.onsuccess = t.step_func(function(e) {
+                assert_equals(e.target.result.property, record.property);
+
+                t.done();
+            });
+        };
+    })
+})
