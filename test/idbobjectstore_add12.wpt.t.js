42d9d9e8e37303fbf2bbe8c0a9318e79beb92e52
Alan Gutierrez <alan@prettyrobots.com>
2021-05-17 03:42:53 -0500

    Fail add extract missing key.
    See #18.

---
+++ test/idbobjectstore_add12.wpt.t.js
@@ -0,0 +1,22 @@
+require('proof')(0, async okay => {
+    await require('./harness')(okay, 'idbobjectstore_add12')
+    await harness(async function () {
+        var db,
+          t = async_test(),
+          record = { property: "data" };
+
+        var open_rq = createdb(t);
+        open_rq.onupgradeneeded = function(e) {
+            db = e.target.result;
+
+            var rq,
+              objStore = db.createObjectStore("store", { keyPath: "key" });
+
+            assert_throws_dom("DataError",
+                function() { rq = objStore.add(record); });
+
+            assert_equals(rq, undefined);
+            t.done();
+        };
+    })
+})
