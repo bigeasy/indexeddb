77afe98332e620fd01eb9f4b300fb59e8f4e6181
Alan Gutierrez <alan@prettyrobots.com>
2021-05-22 01:13:38 -0500

    Get key by index missing.
    See #18.

---
+++ test/idbindex_getKey3.wpt.t.js
@@ -0,0 +1,21 @@
+require('proof')(1, async okay => {
+    await require('./harness')(okay, 'idbindex_getKey3')
+    await harness(async function () {
+        var db,
+          t = async_test();
+
+
+        var open_rq = createdb(t);
+        open_rq.onupgradeneeded = function(e) {
+            db = e.target.result;
+            var rq = db.createObjectStore("test", { keyPath: "key" })
+                       .createIndex("index", "indexedProperty")
+                       .getKey(1);
+
+            rq.onsuccess = t.step_func(function(e) {
+                assert_equals(e.target.result, undefined);
+                t.done();
+            });
+        };
+    })
+})
