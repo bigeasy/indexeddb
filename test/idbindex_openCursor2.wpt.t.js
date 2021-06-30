a84e9572ed1e529a34c6c44bba40191dbdae38f0
Alan Gutierrez <alan@prettyrobots.com>
2021-06-30 10:03:33 -0500

    Index open cursor.
    See #18.

---
+++ test/idbindex_openCursor2.wpt.t.js
@@ -0,0 +1,26 @@
+require('proof')(1, async okay => {
+    await require('./harness')(okay, 'idbindex_openCursor2')
+    await harness(async function () {
+        var db,
+            t = async_test();
+
+        var open_rq = createdb(t);
+        open_rq.onupgradeneeded = function(e) {
+            db = e.target.result;
+            var store = db.createObjectStore("store", { keyPath: "key" });
+            var index = store.createIndex("index", "indexedProperty");
+            store.add({ key: 1, indexedProperty: "data" });
+        }
+        open_rq.onsuccess = function(e) {
+            db = e.target.result;
+            var tx = db.transaction('store');
+            var index = tx.objectStore('store').index('index');
+            tx.abort();
+
+            assert_throws_dom("TransactionInactiveError", function(){
+                index.openCursor();
+            });
+            t.done();
+        }
+    })
+})
