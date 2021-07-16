22a49806f30e99cdc7bdb666c59d6ea942e35cae
Alan Gutierrez <alan@prettyrobots.com>
2021-07-16 13:03:23 -0500

    Values.
    See #18.

---
+++ test/value.wpt.t.js
@@ -0,0 +1,33 @@
+require('proof')(4, async okay => {
+    await require('./harness')(okay, 'value')
+    await harness(async function () {
+        function value(value, _instanceof) {
+            var t = async_test(document.title + " - " + _instanceof.name);
+            t.step(function() {
+                assert_true(value instanceof _instanceof, "TEST ERROR, instanceof");
+            });
+
+            createdb(t).onupgradeneeded = function(e) {
+                e.target.result
+                        .createObjectStore("store")
+                        .add(value, 1);
+
+                e.target.onsuccess = t.step_func(function(e) {
+                    e.target.result
+                            .transaction("store")
+                            .objectStore("store")
+                            .get(1)
+                            .onsuccess = t.step_func(function(e)
+                    {
+                        assert_true(e.target.result instanceof _instanceof, "instanceof")
+                        t.done();
+                    });
+                });
+            };
+        }
+
+        value(new Date(), Date);
+        value(new Array(), Array);
+
+    })
+})
